b0505805d3d6d6cbc5ca11634171f1416deec3f4
ilham adimuslim <ilhamadim90@gmail.com>
2024-06-28 21:29:45 +0700

    deployment

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+import os
 
 app = Flask(_name_)
 api = Api(app)
 
 from routes import *
 
-if _name== "main_":
-   app.run(debug=True)
+if _name_ == "_main_":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
