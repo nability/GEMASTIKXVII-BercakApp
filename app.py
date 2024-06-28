6460d9ff180bade5a6fa9a873e0ed2ae7e54419d
ilham adimuslim <ilhamadim90@gmail.com>
2024-06-28 21:05:57 +0700

    deployment

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from flask import Flask, session, flash, redirect, url_for, render_template
+from flask import Flask
 from flask_restful import Api
-from flask_mysqldb import MySQL
-from flask_bcrypt import Bcrypt
-# from utils_flask import capture_image
 
-app = Flask(__name__)
+app = Flask(_name_)
 api = Api(app)
-bcrypt = Bcrypt(app)
-db = MySQL(app)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'db_gemastik'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-app.config["SECRET_KEY"] = 'RahasiaKabupatenSukabumi'
-app.config['ENV'] = 'development'
-app.config['DEBUG'] = True
 
 from routes import *
 
-if __name__== "__main__":
-   # capture_image()
+if _name== "main_":
    app.run(debug=True)
